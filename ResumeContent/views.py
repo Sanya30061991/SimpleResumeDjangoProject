fc98c6f72ba27c59d815000c417c5f75ec76b551
Alexander <youdoit@mail.ru>
2020-10-02 03:23:04 +0600

    Finished work on sites page

--- ResumeContent/views.py
+++ ResumeContent/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def skill(request):
+    return render(request, 'ResumeContent/skills.html')
+
 def webPages(request):
     return render(request, 'ResumeContent/sites-info.html')
 
